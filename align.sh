f221d5a2da7954695aedf9af41dbedeba9dae7d3
Albert Aparicio <albert.aparicio.isarn@alu-etsetb.upc.edu>
2016-10-22 13:18:22 +0200

    Fix 'align.sh' to properly show columns
    Fix 'align.sh' so the left colummn indicates source's frames and the
    right column indicates target's frames

--- align.sh
+++ align.sh
@@ -49,12 +49,13 @@ while read FILENAME <&3; do
     ahocoder16_64 $DIR_TST/$FILENAME.wav $DIR_VOC/$DIR_TST/$FILENAME.f0 $DIR_VOC/$DIR_TST/$FILENAME.mfcc $DIR_VOC/$DIR_TST/$FILENAME.vf
 
     # Apply dynamic time warping
-    dtw -l 40 -v $DIR_FRM/$FILENAME.frames $DIR_VOC/$DIR_REF/$FILENAME.mfcc < $DIR_VOC/$DIR_TST/$FILENAME.mfcc > /dev/null
+    dtw -l 40 -v $DIR_FRM/$FILENAME.frames $DIR_VOC/$DIR_TST/$FILENAME.mfcc < $DIR_VOC/$DIR_REF/$FILENAME.mfcc > /dev/null
 
     # Convert frames file to ASCII format
     x2x +ia $DIR_FRM/$FILENAME.frames | do_columns.pl -c 2 > $DIR_FRM/$FILENAME.txt
 
     # TODO remove .frames file
+    rm $DIR_FRM/$FILENAME.frames
 done 3< basenames.list
 
 # Set End time of execution
