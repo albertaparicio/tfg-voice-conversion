9278f948150ab54eba23c2ea696b9f56d61bd4d0
Albert Aparicio <albert.aparicio.isarn@gmail.com>
2016-10-23 12:39:06 +0200

    Python module to build a datatable from one file
    Created the module 'utils.py', licensed LGPL v3+

--- data/construct_table.py
+++ data/construct_table.py
@@ -3,74 +3,48 @@
 # This import makes Python use 'print' as in Python 3.x
 from __future__ import print_function
 
-from collections import namedtuple
-
 import numpy as np
 
+from utils import kronecker_delta
 
-def read_params(param_len, source_path, target_path, dtw_path):
-    """This function takes as its inputs:
 
-        param_len: The length of the parameters vector of each vocoder sound file
-        source_path: The file path of the source data (in ASCII, and each file's parameters are located in a single row)
-        target_path: The file path of the target data (in ASCII, and each file's parameters are located in a single row)
-        dtw_path: The file path of the DTW frame matchings (in 2 ASCII columns, the left one being the source
-                    and the right one, the target)
+def parse_file(param_len, file_path):
+    """This function parses a vocoder data file.
 
-        The outputs are:
+    INPUTS:
 
-        source_params: A NumPy's 'ndarray' with the source's parameters organized by rows.
-                        Its size is len(source_path) -i.e. the number of source files- x param_len.
-        target_params: A NumPy's 'ndarray' with the target's parameters organized by rows.
-                        Its size is len(target_path) -i.e. the number of target files- x param_len.
-        dtw_frames:    A NumPy's 'ndarray' with the DTW matchings organized by rows.
-                        Its size is len(source_path) (i.e. the number of source files) x 2."""
+        param_len: The length of the parameters vector of each vocoder frame
+        file_path: The file path of the data (each frame's parameters are located in a single row)
+
+    OUTPUTS:
+
+        file_params: A NumPy's 'ndarray' with the file's parameters organized by rows.
+                        Its size is len(source_path) -i.e. the number of source files- x param_len."""
 
     # Source
-    source_file = open(source_path, 'r')
-    source_lines = source_file.readlines()
+    open_file = open(file_path, 'r')
+    file_lines = open_file.readlines()
 
-    source_params = np.empty([len(source_lines), param_len])
-    for index in range(0, len(source_lines) - 1, 1):
-        aux = source_lines[index].split('\n')
-        source_params[index, :] = aux[0].split('\t')
+    file_params = np.empty([len(file_lines), param_len])
+    for index in range(0, len(file_lines) - 1, 1):
+        aux = file_lines[index].split('\n')
+        file_params[index, :] = aux[0].split('\t')
 
-    del source_file
-    del source_lines
-
-    # Target
-    target_file = open(target_path, 'r')
-    target_lines = target_file.readlines()
-
-    target_params = np.empty([len(target_lines), param_len])
-    for index in range(0, len(target_lines) - 1, 1):
-        aux = target_lines[index].split('\n')
-        target_params[index, :] = aux[0].split('\t')
-
-    del target_file
-    del target_lines
-
-    # DTW Frames
-    frames_file = open(dtw_path, 'r')
-    frames_lines = frames_file.readlines()
-
-    dtw_frames = np.empty([len(frames_lines), 2])
-    for index in range(0, len(frames_lines) - 1, 1):
-        aux = frames_lines[index].split('\n')
-        dtw_frames[index, :] = aux[0].split('\t')
-
-    del frames_file
-    del frames_lines
-
-    # Organize output variables in a tuple
-    ret_params = namedtuple('Params', ['dtw', 'source', 'target'])
-    return ret_params(dtw=dtw_frames, source=source_params, target=target_params)
+    return file_params
 
 
 def align_frames(dtw_frames, source_params, target_params):
     """This functions aligns the source and target frames according to the rows of 'dtw_frames'.
 
-    The inputs of this function are the outputs of the 'read_params'function."""
+    INPUTS:
+        dtw_frames: 2-column 'ndarray' with the DTW matching for each frame
+        source_params: 'ndarray' with the concatenated parameters of the source file
+        target_params: 'ndarray' with the concatenated parameters of the target file
+
+    OUTPUT:
+        An 'ndarray' with the aligned data.
+
+    NOTE: The length of the aligned array may be greater than both source and target files"""
 
     assert source_params.shape[1] == target_params.shape[1]
 
@@ -84,15 +58,58 @@ def align_frames(dtw_frames, source_params, target_params):
     return data
 
 
-# params = read_params(1, 'vocoded/test_source/SF1_TF1_200001.lf0.dat', 'vocoded/test_target/SF1_TF1_200001.lf0.dat',
-#                 'frames/SF1_TF1_200001.frames.txt')
+def build_datatable(basename, source_dir, target_dir, dtw_dir):
+    """This function builds a datatable from the aligned vocoder frames.
+    It reads and parses the input files, concatenates the data and aligns it.
 
-params = read_params(40, 'vocoded/test_source/SF1_TF1_200001.mcp.dat', 'vocoded/test_target/SF1_TF1_200001.mcp.dat',
-                     'frames/SF1_TF1_200001.frames.txt')
+    INPUTS:
+        basename: the name without extension of the file to be prepared
+        source_dir: directory path to the source files. (it must end in '/')
+        target_fir: directory path to the target files. (it must end in '/')
+        dtw_dir: directory path to the DTW frame matchings file. (it must end in '/')"""
 
-data_table = align_frames(params[0], params[1], params[2])
+    # Parse files
+    source_f0 = parse_file(1, source_dir + basename + '.' + 'lf0' + '.dat')
+    source_mcp = parse_file(40, source_dir + basename + '.' + 'mcp' + '.dat')
+    source_vf = parse_file(1, source_dir + basename + '.' + 'vf' + '.dat')
 
-np.savetxt('datatable.csv', data_table, delimiter=',')
-print('done')
-# params = read_params(1, 'vocoded/test_source/SF1_TF1_200001.vf.dat', 'vocoded/test_target/SF1_TF1_200001.vf.dat',
-#                 'frames/SF1_TF1_200001.frames.txt')
+    target_f0 = parse_file(1, target_dir + basename + '.' + 'lf0' + '.dat')
+    target_mcp = parse_file(40, target_dir + basename + '.' + 'mcp' + '.dat')
+    target_vf = parse_file(1, target_dir + basename + '.' + 'vf' + '.dat')
+
+    dtw_frames = parse_file(2, dtw_dir + basename + '.frames.txt')
+
+    # Build voiced/unvoiced flag arrays
+    # The flags are:
+    #   1 -> voiced
+    #   0 -> unvoiced
+    assert source_vf.shape == source_f0.shape
+    source_voiced = np.empty(source_vf.shape)
+    for index, vf in enumerate(source_vf):
+        source_voiced[index] = 1 - kronecker_delta(source_vf[index])
+
+    assert target_vf.shape == target_f0.shape
+    target_voiced = np.empty(target_vf.shape)
+    for index, vf in enumerate(target_vf):
+        target_voiced[index] = 1 - kronecker_delta(target_vf[index])
+
+    # Concatenate source and target params
+    source_params = np.concatenate((
+        source_mcp,
+        source_f0,
+        source_vf,
+        source_voiced
+    ), axis=1)
+
+    target_params = np.concatenate((
+        target_mcp,
+        target_f0,
+        target_vf,
+        target_voiced
+    ), axis=1)
+
+    # Align parameters
+    return align_frames(dtw_frames, source_params, target_params)
+
+# file_datatable = build_datatable('SF1_TF1_200001', 'vocoded/test_source/', 'vocoded/test_target/', 'frames/')
+# np.savetxt('datatable.csv', file_datatable, delimiter=',')
