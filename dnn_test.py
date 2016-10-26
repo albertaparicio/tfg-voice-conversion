3c1510f6ac468c0ccd04b517b1ed8e324f886957
Albert Aparicio <albert.aparicio.isarn@alu-etsetb.upc.edu>
2016-10-26 17:34:21 +0200

    WIP - DNN for MVF mapping

--- dnn_test.py
+++ dnn_test.py
@@ -63,9 +63,9 @@ else:
     print('done')
 
 # TODO adjust sizes and other constants
-batch_size = 32
+batch_size = 300
 # nb_classes = 10
-nb_epochs = 200
+nb_epochs = 100
 hidden_units = 10
 
 learning_rate = 1e-6
@@ -82,47 +82,20 @@ trg_train_frames = train_data[0:17500, 84:86]  # Target data
 src_valid_frames = train_data[17500:train_data.shape[0], 41:43]  # Source data
 trg_valid_frames = train_data[17500:train_data.shape[0], 84:86]  # Target data
 
-# exit()
-#
-# # the data, shuffled and split between train and test sets
-# # X -> data
-# # y -> labels
-# (X_train, y_train), (X_test, y_test) = mnist.load_data()
-#
-# X_train = X_train.reshape(X_train.shape[0], -1, 1)
-# X_test = X_test.reshape(X_test.shape[0], -1, 1)
-# X_train = X_train.astype('float32')
-# X_test = X_test.astype('float32')
-# X_train /= 255
-# X_test /= 255
-# print('X_train shape:', X_train.shape)
-# print(X_train.shape[0], 'train samples')
-# print(X_test.shape[0], 'test samples')
-#
-# # convert class vectors to binary class matrices
-# Y_train = np_utils.to_categorical(y_train, nb_classes)
-# Y_test = np_utils.to_categorical(y_test, nb_classes)
-
 # TODO Define a fully-connected DNN
-print('Evaluate IRNN...')
+print('Evaluate DNN...')
 model = Sequential()
-# model.add(SimpleRNN(output_dim=hidden_units,
-#                     init=lambda shape, name: normal(shape, scale=0.001, name=name),
-#                     inner_init=lambda shape, name: identity(shape, scale=1.0, name=name),
-#                     activation='relu',
-#                     input_shape=X_train.shape[1:]))
-model.add(Dense(4, input_dim=2, activation='relu'))
+model.add(Dense(64, input_dim=2, activation='relu'))
+model.add(Dense(64, activation='relu'))
 model.add(Dense(2, activation='relu'))
 # model.add(Activation('softmax'))
+# sgd = SGD(lr=learning_rate, decay=1e-6, momentum=0.9, nesterov=True)
 rmsprop = RMSprop(lr=learning_rate)
-model.compile(loss='mse',
-              optimizer=rmsprop,
-              metrics=['accuracy'])
+model.compile(loss='mse', optimizer=rmsprop)
 
-# model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epochs, verbose=1, validation_data=(X_test, Y_test))
-model.fit(src_train_frames, trg_train_frames, batch_size=batch_size, nb_epoch=nb_epochs,
+history = model.fit(src_train_frames, trg_train_frames, batch_size=batch_size, nb_epoch=nb_epochs,
           verbose=1, validation_data=(src_valid_frames, trg_valid_frames))
-
+np.savetxt('history.csv', history, delimiter=',')
 # scores = model.evaluate(X_test, Y_test, verbose=0)
 scores = model.evaluate(test_data[:, 41:43], test_data[:, 84:86], verbose=0)
 
