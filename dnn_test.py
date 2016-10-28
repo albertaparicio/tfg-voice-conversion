aed1b515e8117fd0709aa139c2d6d8af6ee65d50
Albert Aparicio <albert.aparicio.isarn@gmail.com>
2016-10-28 16:30:42 +0200

    Add error metrics and save model

--- dnn_test.py
+++ dnn_test.py
@@ -11,10 +11,14 @@ This is a test script for initializing and training a fully-connected DNN
 from __future__ import print_function
 
 import numpy as np
+from keras.callbacks import ReduceLROnPlateau
 from keras.layers import Dense, Dropout
 from keras.layers.advanced_activations import LeakyReLU
 from keras.models import Sequential
 from keras.optimizers import SGD
+from matplotlib import pyplot as plt
+
+from error_metrics import RMSE
 
 # from keras.datasets import mnist
 # from keras.initializations import normal, identity
@@ -70,7 +74,7 @@ batch_size = 300
 nb_epochs = 1000
 hidden_units = 10
 
-learning_rate = 1e-6
+learning_rate = 1e-5
 clip_norm = 1.0
 
 # Split into train and validation (17500 train, 2500 validation)
@@ -81,6 +85,8 @@ np.random.shuffle(train_data)
 src_train_frames = train_data[0:17500, 41:43]  # Source data
 trg_train_frames = train_data[0:17500, 84:86]  # Target data
 
+np.savetxt('src_train_frames.csv', src_train_frames, delimiter=',')
+
 src_valid_frames = train_data[17500:train_data.shape[0], 41:43]  # Source data
 trg_valid_frames = train_data[17500:train_data.shape[0], 84:86]  # Target data
 
@@ -90,7 +96,6 @@ src_train_std = np.std(src_train_frames[:, 0], axis=0)
 trg_train_mean = np.mean(trg_train_frames[:, 0], axis=0)
 trg_train_std = np.std(trg_train_frames[:, 0], axis=0)
 
-
 src_train_frames[:, 0] = (src_train_frames[:, 0] - src_train_mean) / src_train_std
 trg_train_frames[:, 0] = (trg_train_frames[:, 0] - trg_train_mean) / trg_train_std
 
@@ -98,6 +103,8 @@ trg_train_frames[:, 0] = (trg_train_frames[:, 0] - trg_train_mean) / trg_train_s
 src_valid_frames[:, 0] = (src_valid_frames[:, 0] - src_train_mean) / src_train_std
 # trg_valid_frames[:, 0] = trg_valid_frames[:, 0] - np.mean(trg_valid_frames[:, 0], axis=0)
 trg_valid_frames[:, 0] = (trg_valid_frames[:, 0] - trg_train_mean) / trg_train_std
+
+np.savetxt('src_train_frames_nomean.csv', src_train_frames, delimiter=',')
 
 # TODO Define a fully-connected DNN
 print('Evaluate DNN...')
@@ -118,8 +125,12 @@ sgd = SGD(lr=learning_rate, decay=1e-6, momentum=0.9, clipnorm=10)
 
 model.compile(loss='mse', optimizer=sgd)
 
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, mode='min')
 history = model.fit(src_train_frames, trg_train_frames, batch_size=batch_size, nb_epoch=nb_epochs,
-                    verbose=1, validation_data=(src_valid_frames, trg_valid_frames))
+                    verbose=1, validation_data=(src_valid_frames, trg_valid_frames), callbacks=[reduce_lr])
+
+model.save('dnn_model.h5')  # creates a HDF5 file 'my_model.h5'
+# del model  # deletes the existing model
 
 # plt.plot(history.epoch, history.history['loss'], history.epoch, history.history['val_loss'], '--', linewidth=2)
 # plt.legend(['Training loss', 'Validation loss'])
@@ -131,11 +142,45 @@ np.savetxt('epoch.csv', history.epoch, delimiter=',')
 np.savetxt('loss.csv', history.history['loss'], delimiter=',')
 np.savetxt('val_loss.csv', history.history['val_loss'], delimiter=',')
 
-scores = model.evaluate(test_data[:, 41:43], test_data[:, 84:86], verbose=0)
+# scores = model.evaluate(test_data[:, 41:43], test_data[:, 84:86], verbose=0)
+#
+# print(scores)
 
-print(scores)
+# # RMSE of test data
+# test_mean = np.mean(test_data[:, 41], axis=0)
+# test_std = np.std(test_data[:, 41], axis=0)
+#
+# test_data[:, 41] = (test_data[:, 41] - test_mean) / test_std
+# prediction = model.predict(test_data[:, 41:43])
+#
+# prediction[:, 0] = (prediction[:, 0] * test_std) + test_mean
+# test_data[:, 41] = (test_data[:, 41] * test_std) + test_mean
+#
+# rmse_res = RMSE(test_data[:, 41:43], prediction)
+
+# RMSE of training data
+prediction = model.predict(src_train_frames)
+
+prediction[:, 0] = (prediction[:, 0] * src_train_std) + src_train_mean
+src_train_frames[:, 0] = (src_train_frames[:, 0] * src_train_std) + src_train_mean
+
+rmse_res = RMSE(src_train_frames, prediction)
+# np.savetxt('rmse.csv', rmse_res, delimiter=',')
+
+print(rmse_res[0])
+
+# Histogram of predicted training data and training data itself
+plt.hist(prediction[:, 0], bins=100)
+plt.title('Prediction frames')
+plt.show()
+
+# Histogram of training samples
+plt.figure()
+plt.hist(src_train_frames[:, 0], bins=100)
+plt.title('Training frames')
+plt.show()
 
 # TODO Print DNN scores
 # print('IRNN test score:', scores[0])
 # print('IRNN test accuracy:', scores[1])
-# exit()
+exit()
---
+++ error_metrics.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+import numpy as np
+from sklearn.metrics import f1_score, precision_score, recall_score
+
+
+def RMSE(groundtruth, prediction, mask=None):
+    """
+    groundtruth: matrix containing the real samples to be predicted (N samples,
+                 sample dim) 
+    prediction: matrix containing the prediction (N samples,
+                sample dim) 
+    mask: optional binary mask to not consider certain samples
+          (0 in mask)
+    """
+    groundtruth = np.array(groundtruth, dtype=np.float32)
+    prediction = np.array(prediction, dtype=np.float32)
+    assert groundtruth.shape == prediction.shape
+    if mask:
+        mask = np.array(mask)
+        groundtruth = groundtruth[mask == 1]
+        prediction = prediction[mask == 1]
+    D = (groundtruth-prediction)**2
+    D = np.mean(D, axis=0)
+    return np.sqrt(D)
+
+
+def AFPR(groundtruth, prediction):
+    """
+    Evaluate Accuracy, F-measure, Precision and Recall for binary inputs
+    """
+    groundtruth = np.array(groundtruth)
+    prediction = np.array(prediction)
+    assert groundtruth.shape == prediction.shape
+    # A: accuracy
+    I = np.mean(groundtruth == prediction)
+    F = f1_score(groundtruth, prediction)
+    P = precision_score(groundtruth, prediction)
+    R = recall_score(groundtruth, prediction)
+    return I, F, P, R
+
+
+def MCD(gt_cep, pr_cep):
+    """
+    Mel Cepstral Distortion
+    Input are matrices with shape (time, cc_dim)
+    """
+    MCD = 0
+    for t in xrange(gt_cep.shape[0]):
+        acum = 0
+        for n in xrange(gt_cep.shape[1]):
+            acum += (gt_cep[t, n]-pr_cep[t, n])**2
+        MCD += np.sqrt(acum)
+    # scale factor
+    alpha = ((10.*np.sqrt(2))/(gt_cep.shape[0]*np.log(10)))
+    MCD = alpha*MCD
+    return MCD
