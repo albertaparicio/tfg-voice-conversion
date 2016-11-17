f328e11000156ac51de8f499b31c380c76b2246d
Albert Aparicio <albert.aparicio.isarn@gmail.com>
2016-11-17 11:57:24 +0100

    Reformat 'time_dtw'

--- lib/time_dtw.py
+++ lib/time_dtw.py
@@ -7,9 +7,10 @@
 # This script expects the path to the .frames.txt file to be passed as an
 # argument
 
+from sys import argv
+
 import numpy as np
 from tfglib.construct_table import parse_file
-from sys import argv
 
 # Check that a file parameter has been passed
 assert len(argv) == 2
@@ -21,7 +22,4 @@ frames = 0.005 * parse_file(2, argv[1])
 sp = argv[1].split('.')
 
 # Output the resulting frames
-# np.savetxt(sp[0]+'.frames.dtw', frames, fmt='%.3e', delimiter='\t', header='# REF\tTST\n# ----\t----')
-np.savetxt(sp[0]+'.frames.dtw', frames, fmt='%.3f', delimiter='\t', header='REF\tTST\n----\t----')
-# exit()
-
+np.savetxt(sp[0] + '.frames.dtw', frames, fmt='%.3f', delimiter='\t', header='REF\tTST\n----\t----')
