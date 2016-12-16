503b3e9bc7f4e95ec59ddb47ecb4e5a8eacd0b78
Albert Aparicio <albert.aparicio.isarn@gmail.com>
2016-12-16 18:36:49 +0100

    Implement 2-output model

--- seq2seq.py
+++ seq2seq.py
@@ -11,8 +11,9 @@ import h5py
 import numpy as np
 import tfglib.seq2seq_datatable as s2s
 from keras.layers import GRU, Dropout
+from keras.layers import Input
 from keras.layers.core import RepeatVector
-from keras.models import Sequential
+from keras.models import Model
 from keras.optimizers import Adam
 from keras.utils.generic_utils import Progbar
 from tfglib.seq2seq_normalize import maxmin_scaling
@@ -39,7 +40,7 @@ validation_fraction = 0.25
 # Switch to decide if datatable must be built or can be loaded from a file
 build_datatable = False
 
-print('Starting...')
+print('Preparing data\n' + '=' * 8 * 5)
 if build_datatable:
     # Build datatable of training and test data
     # (data is already encoded with Ahocoder)
@@ -136,31 +137,51 @@ trg_valid_masks_f = trg_train_masks[int(np.floor(
 ################
 # Define Model #
 ################
-print('Initializing model')
-model = Sequential()
+print('Initializing model\n' + '=' * 8 * 5)
+main_input = Input(shape=(max_train_length, data_dim),
+                   dtype='float32',
+                   name='main_input'
+                   )
+encoder_GRU = GRU(
+    output_dim=100,
+    input_shape=(max_train_length, data_dim),
+    return_sequences=False,
+    consume_less='gpu'
+)(main_input)
 
-# Encoder Layer
-model.add(GRU(100,
-              input_shape=(max_train_length, data_dim),
-              return_sequences=False,
-              consume_less='gpu'
-              ))
-model.add(RepeatVector(max_train_length))
+repeat_layer = RepeatVector(max_train_length)(encoder_GRU)
 
-# Decoder layer
-model.add(GRU(100, return_sequences=True, consume_less='gpu'))
-model.add(Dropout(0.5))
-model.add(GRU(
-    output_dim,
+decoder_GRU = GRU(100, return_sequences=True, consume_less='gpu')(repeat_layer)
+
+dropout_layer = Dropout(0.5)(decoder_GRU)
+
+parameters_GRU = GRU(
+    output_dim - 2,
     return_sequences=True,
     consume_less='gpu',
-    activation='linear'
-))
+    activation='linear',
+    name='params_output'
+)(dropout_layer)
 
-optimizer = 'adam'
+flags_GRU = GRU(
+    2,
+    consume_less='gpu',
+    activation='sigmoid',
+    name='flags_output'
+)(dropout_layer)
+
+model = Model(input=main_input, output=[parameters_GRU, flags_GRU])
+
+optimizer_name = 'adam'
 adam = Adam(clipnorm=10)
-loss = 'mse'
-model.compile(loss=loss, optimizer=adam, sample_weight_mode="temporal")
+params_loss = 'mse'
+flags_loss = 'binary_crossentropy'
+
+model.compile(optimizer=adam,
+              loss={'params_output': params_loss,
+                    'flags_output': flags_loss},
+              sample_weight_mode="temporal"
+              )
 
 ###############
 # Train model #
@@ -189,15 +210,24 @@ for epoch in range(nb_epochs):
                       index * batch_size:(index + 1) * batch_size]
 
         epoch_train_partial_loss.append(
-            model.train_on_batch(src_batch, trg_batch,
-                                 sample_weight=batch_masks))
+            model.train_on_batch(
+                {'main_input': src_batch},
+                {'params_output': trg_batch[:, 0:42],
+                 'flags_output': trg_batch[:, 42:44]},
+                sample_weight=batch_masks
+            )
+        )
 
         progress_bar.update(index + 1)
 
-    epoch_val_loss = model.evaluate(src_valid_data, trg_valid_data,
-                                    batch_size=batch_size,
-                                    sample_weight=trg_valid_masks_f,
-                                    verbose=0)
+    epoch_val_loss = model.evaluate(
+        src_valid_data,
+        {'params_output': trg_valid_data[:, 0:42],
+         'flags_output': trg_valid_data[:, 42:44]},
+        batch_size=batch_size,
+        sample_weight=trg_valid_masks_f,
+        verbose=0
+    )
 
     epoch_train_loss = np.mean(np.array(epoch_train_partial_loss), axis=0)
 
@@ -211,36 +241,43 @@ for epoch in range(nb_epochs):
           )
     print()
 
-print('Saving model')
-model.save_weights('models/seq2seq_' + loss + '_' + optimizer + '_epochs_' +
-                   str(nb_epochs) + '_lr_' + str(learning_rate) +
-                   '_weights.h5')
+###############
+# Saving data #
+###############
+print('Saving model\n' + '=' * 8 * 5)
+model.save_weights(
+    'models/seq2seq_' + params_loss + '_' + flags_loss + '_' + optimizer_name +
+    '_epochs_' + str(nb_epochs) + '_lr_' + str(learning_rate) + '_weights.h5')
 
-with open('models/seq2seq_' + loss + '_' + optimizer + '_epochs_' +
-          str(nb_epochs) + '_lr_' + str(learning_rate) + '_model.json',
-          'w'
+with open('models/seq2seq_' + params_loss + '_' + flags_loss + '_' +
+          optimizer_name + '_epochs_' + str(nb_epochs) + '_lr_' +
+          str(learning_rate) + '_model.json', 'w'
           ) as model_json:
     model_json.write(model.to_json())
 
-print('Saving training parameters')
+print('Saving training parameters\n' + '=' * 8 * 5)
 with h5py.File('training_results/seq2seq_training_params.h5', 'w') as f:
-    f.attrs.create('loss', np.string_(loss))
-    f.attrs.create('optimizer', np.string_(optimizer))
+    f.attrs.create('params_loss', np.string_(params_loss))
+    f.attrs.create('flags_loss', np.string_(flags_loss))
+    f.attrs.create('optimizer', np.string_(optimizer_name))
     f.attrs.create('epochs', nb_epochs, dtype=int)
     f.attrs.create('learning_rate', learning_rate)
     f.attrs.create('train_speakers_max', train_speakers_max)
     f.attrs.create('train_speakers_min', train_speakers_min)
 
 print('Saving training results')
-np.savetxt('training_results/seq2seq_' + loss + '_' + optimizer + '_epochs_' +
-           str(nb_epochs) + '_lr_' + str(learning_rate) +
-           '_epochs.csv', np.arange(nb_epochs), delimiter=',')
-np.savetxt('training_results/seq2seq_' + loss + '_' + optimizer + '_epochs_' +
-           str(nb_epochs) + '_lr_' + str(learning_rate) +
-           '_loss.csv', training_history, delimiter=',')
-np.savetxt('training_results/seq2seq_' + loss + '_' + optimizer + '_epochs_' +
-           str(nb_epochs) + '_lr_' + str(learning_rate) +
-           '_val_loss.csv', validation_history, delimiter=',')
+np.savetxt('training_results/seq2seq_' + params_loss + '_' + flags_loss + '_' +
+           optimizer_name + '_epochs_' + str(nb_epochs) + '_lr_' +
+           str(learning_rate) + '_epochs.csv',
+           np.arange(nb_epochs), delimiter=',')
+np.savetxt('training_results/seq2seq_' + params_loss + '_' + flags_loss + '_' +
+           optimizer_name + '_epochs_' + str(nb_epochs) + '_lr_' +
+           str(learning_rate) + '_loss.csv',
+           training_history, delimiter=',')
+np.savetxt('training_results/seq2seq_' + params_loss + '_' + flags_loss + '_' +
+           optimizer_name + '_epochs_' + str(nb_epochs) + '_lr_' +
+           str(learning_rate) + '_val_loss.csv',
+           validation_history, delimiter=',')
 
 print('========================' + '\n' +
       '======= FINISHED =======' + '\n' +
