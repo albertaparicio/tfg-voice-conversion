b8489ccd1c1360eda7142a8de995cf058f26ecf8
Albert Aparicio <albert.aparicio.isarn@gmail.com>
2017-03-02 19:13:44 +0100

    Cleanup pretrain_echo_files_list.py

--- pretrain_echo_files_list.py
+++ pretrain_echo_files_list.py
@@ -3,23 +3,18 @@
 
 # This import makes Python use 'print' as in Python 3.x
 from __future__ import print_function
-from tfglib.pretrain_data_params import pretrain_load_data_parameters
+
 from os.path import join
+
 from h5py import File
 
 data_path = 'pretrain_data/test'
-params_file='pretrain_params.h5'
+params_file = 'pretrain_params.h5'
 
 with File(join(data_path, params_file), 'r') as file:
     files_list_encoded = file['files_list'][:]
     test_files_list = [n[0] for n in files_list_encoded]
 
-
-# (_,_,_,test_files_list) = pretrain_load_data_parameters(data_path)
-
 with open(join(data_path, 'pretrain_basenames.list'), 'w') as thefile:
     for item in test_files_list:
         thefile.write("%s\n" % item.decode('utf-8'))
-
-
-# [string.decode('utf-8') for string in test_files_list]
