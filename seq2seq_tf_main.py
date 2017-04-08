7d0dfbe54056b3501a99248be3d993732d73fa56
Albert Aparicio <albert.aparicio.isarn@gmail.com>
2017-04-08 18:31:07 +0200

    TF training with no validation, yet

--- seq2seq_tf_main.py
+++ seq2seq_tf_main.py
@@ -13,12 +13,12 @@ import argparse
 import gzip
 import logging
 import os
+import timeit
 from sys import version_info
-
+import numpy as np
 import tensorflow as tf
-import tfglib.seq2seq_datatable as s2s
 
-from seq2seq_tf_model import Seq2Seq
+from seq2seq_tf_model import Seq2Seq, DataLoader
 
 # Conditional imports
 if version_info.major > 2:
@@ -30,57 +30,33 @@ else:
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 
-def load_dataset(data_path, out_file, save_h5):
-  if save_h5:
-    logging.info('Saving training datatable')
-
-    (src_train_datatable,
-     src_train_masks,
-     trg_train_datatable,
-     trg_train_masks,
-     max_train_length,
-     train_speakers_max,
-     train_speakers_min
-     ) = s2s.seq2seq_save_datatable(data_path, out_file)
-
-    logging.info('DONE Saving training datatable')
-
-  else:
-    print('Load pretraining parameters')
-    (src_train_datatable,
-     src_train_masks,
-     trg_train_datatable,
-     trg_train_masks,
-     max_train_length,
-     train_speakers_max,
-     train_speakers_min
-     ) = s2s.seq2seq2_load_datatable(opts.train_out_file + '.h5')
-
-  train_speakers = train_speakers_max.shape[0]
-
-  return (
-    train_speakers, train_speakers_max, train_speakers_min, max_train_length
-  )
-
-
 def main(args):
   # If-else for training and testing
   if args.do_train:
     # Load training dataset
-    (train_speakers, train_speakers_max, train_speakers_min, max_train_length
-     ) = load_dataset(args.train_data_path, args.train_out_file, args.save_h5)
+    # (src_datatable, trg_datatable, trg_masks, train_spk, train_spk_max,
+    # train_spk_min, max_seq_length) = load_dataset(args.train_data_path,
+    #                                                    args.train_out_file,
+    #                                                    args.save_h5,
+    #                                                    args.val_fraction)
 
+    dl = DataLoader(args)
+
+    # TODO Remove '10' and put max_seq_length in its place
     seq2seq_model = Seq2Seq(args.enc_rnn_layers, args.dec_rnn_layers,
-                            args.rnn_size, max_train_length, args.params_len)
+                            # args.rnn_size, 10, args.params_len)
+                            args.rnn_size, dl.max_seq_length, args.params_len)
 
-    train(args, seq2seq_model)
+    train(args, seq2seq_model, dl)
 
   if args.do_test:
     tf.reset_default_graph()
 
     # Load test dataset
-    (test_speakers, test_speakers_max, test_speakers_min, max_test_length
-     ) = load_dataset(args.test_data_path, args.test_out_file, args.save_h5)
+    (src_test_datatable, src_test_masks, trg_test_datatable, trg_test_masks,
+     test_speakers, test_speakers_max, test_speakers_min,
+     max_test_length) = load_dataset(args.test_data_path, args.test_out_file,
+                                     args.save_h5, )
 
     seq2seq_model = Seq2Seq(args.enc_rnn_layers, args.dec_rnn_layers,
                             args.rnn_size, max_test_length, args.params_len)
@@ -88,12 +64,79 @@ def main(args):
     test(args, seq2seq_model)
 
 
-def train(args, model):
+def train(args, model, dl):
+  with tf.Session() as sess:
+    batch_idx = 0
+    curr_epoch = 0
+    count = 0
+    batch_timings = []
+    try:
+      tf.global_variables_initializer().run()
+      merged = tf.summary.merge_all()
+    except AttributeError:
+      # Backward compatibility
+      tf.initialize_all_variables().run()
+      merged = tf.merge_all_summaries()
+    LOG_DIR = os.path.join(opts.save_path, 'tf_train')
+    train_writer = tf.summary.FileWriter(LOG_DIR, sess.graph)
+    best_val_loss = 10e6
+    curr_patience = opts.patience
+
+    for src_batch, trg_batch, trg_mask in dl.next_batch():
+      if curr_epoch == 0 and batch_idx == 0:
+        batches_per_epoch = dl.batches_per_epoch
+        logging.info('Batches per epoch: {}'.format(batches_per_epoch))
+      beg_t = timeit.default_timer()
+
+      # Model's placeholders
+      feed_dict = {
+        model.gtruth: trg_batch[:, 0:int(model.gtruth.get_shape()[1]), :]}
+
+      for i, enc_in in enumerate(model.encoder_inputs):
+        # TODO Fix encoder_inputs so that it takes 64 parameters as input
+        # feed_dict[enc_in] = src_batch[:,i,:]
+        feed_dict[enc_in] = src_batch[:, i, 0:44]
+
+      # Decoder inputs are trg_batch with the first timestep set to 0 (GO)
+      trg_batch = np.roll(trg_batch, 1, axis=1)
+      trg_batch[:, 0, :] = np.zeros((model.batch_size, model.parameters_length))
+
+      for i, dec_in in enumerate(model.decoder_inputs):
+        feed_dict[dec_in] = trg_batch[:, i, :]
+
+      # tr_loss, _, enc_state, summary = sess.run(
+      #   [model.loss, model.train_op, model.enc_state, merged],
+      #   feed_dict=feed_dict)
+      tr_loss, _ = sess.run(
+        [model.loss, model.train_op],
+        feed_dict=feed_dict)
+
+      batch_timings.append(timeit.default_timer() - beg_t)
+
+      # TODO Use the right variables in this print
+      logging.info('{}/{} (epoch {}) loss {}, time/batch {} s'.format(
+        count,
+        opts.epoch * batches_per_epoch,
+        curr_epoch,
+        tr_loss,
+        np.mean(batch_timings)))
+      if batch_idx % opts.save_every == 0:
+        # train_writer.add_summary(summary, count)
+        checkpoint_file = os.path.join(opts.save_path, 'model.ckpt')
+        model.save(sess, checkpoint_file, count)
+
+      batch_idx += 1
+      count += 1
+      if curr_epoch >= opts.epoch:
+        break
+
   return
 
 
 def evaluate(sess, args, model):
   """ Evaluate an epoch over the given data_loader batches """
+
+  # TODO Cridar run sols amb model.loss, sense train_op
   return
 
 
@@ -111,8 +154,10 @@ if __name__ == '__main__':
   parser.add_argument('--test_data_path', type=str, default="data/test/")
   parser.add_argument('--test_out_file', type=str,
                       default="data/seq2seq_test_datatable")
+  parser.add_argument('--val_fraction', type=float, default=0.25)
   parser.add_argument('--save-h5', dest='save_h5', action='store_true',
                       help='Save dataset to .h5 file')
+  parser.add_argument('--params_len', type=int, default=44)
   parser.add_argument('--patience', type=int, default=4,
                       help="Patience epochs to do validation, if validation "
                            "score is worse than train for patience epochs "
@@ -133,14 +178,14 @@ if __name__ == '__main__':
                       action='store_false', help='Flag to train or not.')
   parser.add_argument('--no-test', dest='do_test',
                       action='store_false', help='Flag to test or not.')
-  parser.add_argument('--save_path', type=str, default="checkpoint")
+  parser.add_argument('--save_path', type=str, default="training_results")
   parser.set_defaults(do_train=True, do_test=True, save_h5=False)
   opts = parser.parse_args()
 
   if not os.path.exists(opts.save_path):
     os.makedirs(opts.save_path)
   # save config
-  with gzip.open(os.path.join(opts.save_path, 'config.pkl.gz'), 'wg') as cf:
+  with gzip.open(os.path.join(opts.save_path, 'config.pkl.gz'), 'wb') as cf:
     pickle.dump(opts, cf)
 
   main(opts)
--- seq2seq_tf_model.py
+++ seq2seq_tf_model.py
@@ -5,13 +5,17 @@
 # encoders and decoders from TensorFlow
 
 # TODO Document and explain steps
+# TODO Move this model to tfglib
 
 # This import makes Python use 'print' as in Python 3.x
 from __future__ import print_function
 
 import logging
 
+import numpy as np
 import tensorflow as tf
+import tfglib.seq2seq_datatable as s2s
+from tfglib.seq2seq_normalize import maxmin_scaling
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
@@ -42,25 +46,42 @@ class Seq2Seq(object):
     self.batch_size = batch_size
     self.clip_norm = clip_norm
     self.learning_rate = learning_rate
-    self.seq_length = seq_length
+
+    # TODO The sequence length should be the actual seq_length of each sequence?
+    # self.seq_length = seq_length
+    # self.seq_length = seq_length * np.ones((batch_size,))
+    self.seq_length = tf.placeholder(tf.int32, [self.batch_size])
+
     self.parameters_length = params_length
     self.gtruth = tf.placeholder(tf.float32,
                                  [self.batch_size,
-                                  self.seq_length,
+                                  seq_length,
                                   self.parameters_length])
-    self.encoder_inputs = tf.placeholder(tf.float32,
-                                         [batch_size,
-                                          self.seq_length,
-                                          self.parameters_length])
-    self.decoder_inputs = tf.placeholder(tf.float32,
-                                         [batch_size,
-                                          self.seq_length,
-                                          self.parameters_length])
-    # attention_states: 3D Tensor [batch_size x attn_length x attn_size]
-    self.attention_states = tf.placeholder(tf.float32,
-                                           [batch_size,
-                                            self.attn_length,
-                                            self.attn_size])
+    #
+    # self.encoder_inputs = tf.placeholder(tf.float32,
+    #                                      [batch_size,
+    #                                       self.seq_length,
+    #                                       self.parameters_length])
+    # inputs: A length T list of inputs, each a Tensor of
+    # shape [batch_size, input_size], or a nested tuple of such elements
+    self.encoder_inputs = [
+      tf.placeholder(tf.float32, [batch_size, self.parameters_length]
+                     ) for _ in range(seq_length)]
+
+    # self.decoder_inputs = tf.placeholder(tf.float32,
+    #                                      [batch_size,
+    #                                       seq_length,
+    #                                       self.parameters_length])
+    # decoder_inputs: A list of 2D Tensors [batch_size x input_size].
+    self.decoder_inputs = [
+      tf.placeholder(tf.float32, [batch_size, self.parameters_length]
+                     ) for _ in range(seq_length)]
+
+    # # attention_states: 3D Tensor [batch_size x attn_length x attn_size]
+    # self.attention_states = tf.placeholder(tf.float32,
+    #                                        [batch_size,
+    #                                         self.attn_length,
+    #                                         self.attn_size])
     self.prediction = self.inference()
     self.loss = self.mse_loss(self.gtruth, self.prediction)
 
@@ -78,7 +99,7 @@ class Seq2Seq(object):
     self.opt = tf.train.AdamOptimizer(self.curr_lr)
 
     self.train_op = self.opt.apply_gradients(zip(grads, tvars))
-    self.enc_state = None  # To be assigned a value in self.inference()
+    self.enc_state = None  # TODO To be assigned a value in self.inference()
 
   @staticmethod
   def build_multirnn_block(rnn_size, rnn_layers, cell_type):
@@ -97,6 +118,8 @@ class Seq2Seq(object):
 
   @staticmethod
   def mse_loss(gtruth, prediction):
+    # Previous to the computation, I gotta mask the predictions
+
     return tf.reduce_mean(tf.squared_difference(gtruth, prediction))
 
   def inference(self):
@@ -111,13 +134,17 @@ class Seq2Seq(object):
                                               self.cell_type)
       self.enc_zero = enc_cell.zero_state(self.batch_size, tf.float32)
 
+      # TODO Check why code is stuck at this line
       enc_out, enc_state = tf_static_rnn(enc_cell,
                                          self.encoder_inputs,
                                          initial_state=self.enc_zero,
                                          sequence_length=self.seq_length)
     # this op is created to visualize the thought vectors
     self.enc_state = enc_state
-    logging.info('enc out tensor shape: ' + enc_out.get_shape())
+    logging.info(
+      'enc out (len {}) tensors shape: {}'.format(
+        len(enc_out), enc_out[0].get_shape()
+      ))
     # print('enc out tensor shape: ', enc_out.get_shape())
 
     dec_cell = Seq2Seq.build_multirnn_block(self.rnn_size,
@@ -137,15 +164,150 @@ class Seq2Seq(object):
     else:
       loop_function = None
 
+    # First calculate a concatenation of encoder outputs to put attention on.
+    # TODO Change array_ops for tf
+    top_states = [
+      tf.reshape(e, [-1, 1, enc_cell.output_size]) for e in enc_out
+    ]
+    attention_states = tf.concat(top_states, 1)
+
     dec_out, dec_state = tf_attention_decoder(
       self.decoder_inputs, enc_state, cell=dec_cell,
-      attention_states=self.attention_states, loop_function=loop_function
+      attention_states=attention_states, loop_function=loop_function
     )
 
     # print('dec_state shape: ', dec_state[0].get_shape())
     # merge outputs into a tensor and transpose to be [B, seq_length, out_dim]
     dec_outputs = tf.transpose(tf.stack(dec_out), [1, 0, 2])
     # print('dec outputs shape: ', dec_outputs.get_shape())
-    logging.info('dec outputs shape: ', dec_outputs.get_shape())
+    logging.info('dec outputs shape: {}'.format(dec_outputs.get_shape()))
 
     return dec_outputs
+
+
+class DataLoader(object):
+  # TODO Finish this class and move it to a new file
+  def __init__(self, args):
+    self.batch_size = args.batch_size
+
+    (self.src_datatable,
+     self.trg_datatable,
+     self.trg_masks,
+     self.train_spk,
+     self.train_spk_max,
+     self.train_spk_min,
+     self.max_seq_length
+     ) = self.load_dataset(
+      args.train_data_path,
+      args.train_out_file,
+      args.save_h5,
+      args.train_out_file,
+      args.val_fraction
+    )
+
+    self.batches_per_epoch = int(
+      np.floor(self.src_datatable.shape[0] / self.batch_size)
+    )
+
+  def load_dataset(self, data_path, out_file, save_h5, train_out_file,
+                   validation_fraction):
+    if save_h5:
+      logging.info('Saving training datatable')
+
+      (src_datatable,
+       src_masks,
+       trg_datatable,
+       trg_masks,
+       max_seq_length,
+       train_speakers_max,
+       train_speakers_min
+       ) = s2s.seq2seq_save_datatable(data_path, out_file)
+
+      logging.info('DONE Saving training datatable')
+
+    else:
+      print('Load pretraining parameters')
+      (src_datatable,
+       src_masks,
+       trg_datatable,
+       trg_masks,
+       max_seq_length,
+       train_speakers_max,
+       train_speakers_min
+       ) = s2s.seq2seq2_load_datatable(train_out_file + '.h5')
+
+    train_speakers = train_speakers_max.shape[0]
+
+    # Normalize data
+    # Iterate over sequence 'slices'
+    assert src_datatable.shape[0] == trg_datatable.shape[0]
+
+    for i in range(src_datatable.shape[0]):
+      (
+        src_datatable[i, :, 0:42],
+        trg_datatable[i, :, 0:42]
+      ) = maxmin_scaling(
+        src_datatable[i, :, :],
+        src_masks[i, :],
+        trg_datatable[i, :, :],
+        trg_masks[i, :],
+        train_speakers_max,
+        train_speakers_min
+      )
+
+    # # TODO Implement choice between returning training data or validation data
+    # ################################################
+    # # Split data into training and validation sets #
+    # ################################################
+    # # #################################
+    # # # TODO COMMENT AFTER DEVELOPING #
+    # # #################################
+    # # batch_size = 2
+    # # nb_epochs = 2
+    # #
+    # # num = 10
+    # # src_datatable = src_datatable[0:num]
+    # # src_masks = src_masks[0:num]
+    # # trg_datatable = trg_datatable[0:num]
+    # # trg_masks = trg_masks[0:num]
+    # #
+    # # model_description = 'DEV_' + model_description
+    # # #################################################
+    #
+    # src_data = src_datatable[0:int(np.floor(
+    #   src_datatable.shape[0] * (1 - validation_fraction)))]
+    # src_valid_data = src_datatable[int(np.floor(
+    #   src_datatable.shape[0] * (1 - validation_fraction))):]
+    #
+    # trg_data = trg_datatable[0:int(np.floor(
+    #   trg_datatable.shape[0] * (1 - validation_fraction)))]
+    # trg_valid_data = trg_datatable[int(np.floor(
+    #   trg_datatable.shape[0] * (1 - validation_fraction))):]
+    #
+    # trg_masks_f = trg_masks[0:int(np.floor(
+    #   trg_masks.shape[0] * (1 - validation_fraction)))]
+    # trg_valid_masks_f = trg_masks[int(np.floor(
+    #   trg_masks.shape[0] * (1 - validation_fraction))):]
+    #
+    # return (src_data, src_valid_data, trg_data, trg_valid_data, trg_masks_f,
+    #         trg_valid_masks_f, train_speakers, train_speakers_max,
+    #         train_speakers_min, max_seq_length)
+
+    return (src_datatable, trg_datatable, trg_masks, train_speakers,
+            train_speakers_max, train_speakers_min, max_seq_length)
+
+  def next_batch(self):
+    batch_id = 0
+    while True:
+      src_batch = self.src_datatable[
+                  batch_id * self.batch_size:(batch_id + 1) * self.batch_size,
+                  :, :]
+      trg_batch = self.trg_datatable[
+                  batch_id * self.batch_size:(batch_id + 1) * self.batch_size,
+                  :, :]
+      trg_mask = self.trg_masks[
+                 batch_id * self.batch_size:(batch_id + 1) * self.batch_size, :]
+
+      batch_id = (batch_id + 1) % self.batches_per_epoch
+
+      yield (src_batch, trg_batch, trg_mask)
